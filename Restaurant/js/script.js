2fbdb8ecd49877f2b1029570f4c10dbda9fdf31b
Pushkar Mohite <mpushkar2005@gmail.com>
2026-07-17 19:42:56 +0530

    Redesign signature menu with luxury interactive layout

--- Restaurant/js/script.js
+++ Restaurant/js/script.js
@@ -19,3 +19,62 @@ window.addEventListener("scroll", () => {
     }
 
 });
+
+const featuredImage = document.getElementById("featuredDishImage");
+
+const defaultImage = "assets/images/pasta.jpg";
+
+const dishes = document.querySelectorAll(".dish-info");
+
+dishes.forEach((dish) => {
+
+    dish.addEventListener("mouseenter", () => {
+
+        // Highlight active dish
+        dishes.forEach(item => item.classList.remove("active"));
+        dish.classList.add("active");
+
+        const newImage = dish.dataset.image;
+
+        // Animate image out
+        featuredImage.classList.add("changing");
+
+        setTimeout(() => {
+
+            featuredImage.src = newImage;
+
+            // Wait until the image has loaded
+            featuredImage.onload = () => {
+
+                featuredImage.classList.remove("changing");
+
+            };
+
+        },250);
+
+    });
+
+});
+
+document.querySelector(".featured-list").addEventListener("mouseleave", () => {
+
+    dishes.forEach(item => item.classList.remove("active"));
+    dishes[0].classList.add("active");
+
+    featuredImage.classList.add("changing");
+
+    setTimeout(() => {
+
+        featuredImage.src = defaultImage;
+
+        featuredImage.onload = () => {
+
+            featuredImage.classList.remove("changing");
+
+        };
+
+    },250);
+
+});
+
+dishes[0].classList.add("active");
